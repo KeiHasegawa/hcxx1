c87055b98ea1e02a68d7e43291b63b2b999f798e
KeiHasegawa <kei.hasegawa.18@gmail.com>
2020-12-17 12:45:46 +0900

    fix template parsing

---
+++ patch.18.pl
@@ -0,0 +1,34 @@
+#State XXX
+#
+# AAA elaborated_type_specifier: typenaming nested_name_specifier CLASS_NAME_LEX .
+# BBB postfix_expression: typenaming nested_name_specifier CLASS_NAME_LEX . '(' fcast_prev2 expression_list ')'
+# CCC                   | typenaming nested_name_specifier CLASS_NAME_LEX . '(' fcast_prev2 ')'
+#
+
+while (<>) {
+    chop;
+    next if ( !/^[Ss]tate (.*)/ );
+    $xxx = $1;
+    $_ = <>;
+    $_ = <>; chop;
+    next if (!/([0-9]+) elaborated_type_specifier: typenaming nested_name_specifier CLASS_NAME_LEX \./);
+    $aaa = $1;
+    $_ = <>; chop;
+    next if (!/([0-9]+) postfix_expression: typenaming nested_name_specifier CLASS_NAME_LEX \./);
+    $bbb = $1;
+    goto label;
+}
+
+print STDERR "Error detected at $0\n";
+exit 1;
+
+label:
+print<<EOF
+  if (yystate == $xxx) {
+    using namespace cxx_compiler::parse;
+    if (peek() != '(') {
+      YYDPRINTF((stderr, "patch.18 is applied\\n"));
+      identifier::mode = identifier::new_obj;
+    }
+  }
+EOF
